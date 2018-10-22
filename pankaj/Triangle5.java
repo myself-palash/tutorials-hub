83627a2c9843d71b2018d734e14b9cc5156b7f01
pankaj <mr.pankaj.dey@gmail.com>
2018-10-22 16:53:00 +0530

    Assessment Triangle

---
+++ pankaj/Triangle5.java
@@ -0,0 +1,14 @@
+class Triangle5{
+	public static void main(String[] args){
+		int i,j,n=5;
+		for(i=0;i<n;i++){
+			for(j=n-1;j>i;j--){
+				System.out.print(" ");
+			}
+			for(j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ pankaj/Triangle6.java
@@ -0,0 +1,23 @@
+class Triangle6{
+	public static void main(String[] args){
+		int i,j,n=5;
+		for(i=0;i<n;i++){
+			for(j=n-1;j>i;j--){
+				System.out.print(" ");
+			}
+			for(j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(i=0;i<n;i++){
+			for(j=0;j<=i;j++){
+				System.out.print(" ");
+			}
+			for(j=n-1;j>i;j--){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
