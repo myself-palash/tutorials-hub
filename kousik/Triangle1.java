9314318b924761f585084ea58a469bcda3d5f13e
kousikpanda2013 <kousikpanda2013.gmail.com>
2018-10-22 16:49:36 +0530

    6 programes are here ,,,please check it if you need

---
+++ kousik/Triangle1.java
@@ -0,0 +1,16 @@
+class Triangle1
+{
+   public static void main(String[] args)
+   {
+   int n=0;
+   for(int i=1;i<10;i++)
+    {
+     for(int j=0;j<i;j++)
+	 {
+	 System.out.print(n+" ");
+	 n++;
+	 }
+	 System.out.println();
+	}
+   }
+ }
